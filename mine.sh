1cda882ea555c07852bba4854ac353e8b6afc541
lnxd <48756329+lnxd@users.noreply.github.com>
2021-04-05 20:48:07 +1000

    Updated to make it evident that xmrig is being run as root

--- mine.sh
+++ mine.sh
@@ -6,9 +6,8 @@ echo "Base:     Ubuntu 20.04"
 echo "Target:   Unraid"
 echo "Donation: ${FEE} ${DONATE}%"
 echo ""
-echo "Running xmrig with the following flags:"
+echo "Running xmrig as $(id) with the following flags:"
 echo "--url=${POOL} --coin=${COIN} --user=${WALLET}.${WORKER} --randomx-wrmsr=-1 --randomx-no-rdmsr ${ADDITIONAL}"
 
 cd /home/docker/xmrig-${FEE}
-./xmrig --url=${POOL} --coin=${COIN} --user=${WALLET}.${WORKER} --donate-level=${DONATE} --randomx-wrmsr=-1 --randomx-no-rdmsr ${ADDITIONAL} 
-
+./xmrig --url=${POOL} --coin=${COIN} --user=${WALLET}.${WORKER} --donate-level=${DONATE} --randomx-wrmsr=-1 --randomx-no-rdmsr ${ADDITIONAL}
