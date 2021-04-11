a1486c28d5031c6746cdd5ed82cff4b4d9866d7f
lnxd <48756329+lnxd@users.noreply.github.com>
2021-04-11 17:34:19 +1000

    Added latest version of cuda

--- mine.sh
+++ mine.sh
@@ -67,8 +67,14 @@ if [[ "${DRIVERV}" != "" ]]; then
 
     nvidia)
         uninstall_driver
-        apt-get install -y nvidia-headless-$NVIDIA_DRIVERV
-        apt-get install -y --no-install-recommends nvidia-cuda-toolkit
+        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+        mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+        apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+        add-apt-repository -y "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+        apt-get update
+        apt-get -y install cuda
+        apt-get -y clean all
+        #apt-get install -y nvidia-headless-$NVIDIA_DRIVERV
         INSTALLED_DRIVERV="NVIDIA"
         ;;
 
